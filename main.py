16ae663d0a3e3cc982de3758added5a97d74e902
Alvyn Abranches <alvynabranches@gmail.com>
2020-07-19 02:30:48 +0530

    19072020v3

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from warnings import filterwarnings as fw
+import requirements
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly
-from warnings import filterwarnings as fw
 fw('ignore')
 
 # TASK 1
--- requirements.py
+++ requirements.py
@@ -3,8 +3,20 @@ import subprocess
 try:
     import numpy as _
 except Exception as e:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--user', 'numpy'])
 try:
     import pandas as _
 except Exception as e:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--user', 'pandas'])
+try:
+    import matplotlib as _
+except Exception as e:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--user', 'matplotlib'])
+try:
+    import seaborn as _
+except Exception as e:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--user', 'seaborn'])
+try:
+    import plotly as _
+except Exception as e:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--user', 'plotly'])
